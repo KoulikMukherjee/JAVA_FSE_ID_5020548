ca1cd21aa57b989d2cfb9a7b561eb6728634dd15
KoulikMukherjee <148150992+KoulikMukherjee@users.noreply.github.com>
2024-07-29 22:28:22 +0530

    Add files via upload

---
+++ Week 1/Design Pattern/SingletonPatternExample/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Test test1 = new Test();
+        Test test2 = new Test();
+        //If the hashcode is same for both the objects then it is following a Singleton Pattern
+    }
+}
---
+++ Week 1/Design Pattern/SingletonPatternExample/src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public Test(){
+        Logger logger = Logger.getInstance();
+        System.out.println(logger.hashCode());
+    }
+}

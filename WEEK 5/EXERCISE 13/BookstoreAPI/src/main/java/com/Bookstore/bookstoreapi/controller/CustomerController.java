11dfdb809ca86cd24900d615fa8b76853d8c3c56
KoulikMukherjee <148150992+KoulikMukherjee@users.noreply.github.com>
2024-08-27 14:33:46 +0530

    Add files via upload

---
+++ WEEK 5/EXERCISE 13/BookstoreAPI/src/main/java/com/Bookstore/bookstoreapi/controller/CustomerController.java
@@ -0,0 +1,59 @@
+package com.Bookstore.bookstoreapi.controller;
+
+import com.Bookstore.bookstoreapi.assembler.CustomerResourceAssembler;
+import com.Bookstore.bookstoreapi.dto.CustomerDTO;
+import com.Bookstore.bookstoreapi.service.CustomerService;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+    private final CustomerResourceAssembler customerResourceAssembler;
+
+    public CustomerController(CustomerService customerService, CustomerResourceAssembler customerResourceAssembler) {
+        this.customerService = customerService;
+        this.customerResourceAssembler = customerResourceAssembler;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<CustomerDTO>> getCustomerById(@PathVariable Long id) {
+        CustomerDTO customerDTO = customerService.getCustomerById(id);
+        EntityModel<CustomerDTO> customerResource = customerResourceAssembler.toModel(customerDTO);
+        return ResponseEntity.ok(customerResource);
+    }
+
+    @PostMapping
+    public ResponseEntity<EntityModel<CustomerDTO>> createCustomer(@RequestBody CustomerDTO customerDTO) {
+        CustomerDTO createdCustomer = customerService.createCustomer(customerDTO);
+        EntityModel<CustomerDTO> customerResource = customerResourceAssembler.toModel(createdCustomer);
+        return ResponseEntity.created(customerResource.getRequiredLink("self").toUri()).body(customerResource);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<CustomerDTO>> updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
+        CustomerDTO updatedCustomer = customerService.updateCustomer(id, customerDTO);
+        EntityModel<CustomerDTO> customerResource = customerResourceAssembler.toModel(updatedCustomer);
+        return ResponseEntity.ok(customerResource);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EntityModel<CustomerDTO>>> getAllCustomers() {
+        List<EntityModel<CustomerDTO>> customerResources = customerService.getAllCustomers().stream()
+                .map(customerResourceAssembler::toModel)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(customerResources);
+    }
+}

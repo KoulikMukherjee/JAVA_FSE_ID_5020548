bcae79f2241edef630febd374a70b2e314b58b6a
KoulikMukherjee <148150992+KoulikMukherjee@users.noreply.github.com>
2024-08-27 15:04:06 +0530

    Add files via upload

---
+++ WEEK 5/EXERCISE 14/BookstoreAPI/src/main/java/com/Bookstore/bookstoreapi/log/AuthenticationResponse.java
@@ -0,0 +1,24 @@
+package com.Bookstore.bookstoreapi.log;
+
+public class AuthenticationResponse {
+
+    private String jwt;
+
+    // Default constructor (required for JSON deserialization)
+    public AuthenticationResponse() {
+    }
+
+    // Parameterized constructor
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    // Getter and Setter
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}

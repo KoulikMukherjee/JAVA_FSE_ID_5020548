eef939ff862c0a5756541bb7bc54137a049f37c2
KoulikMukherjee <148150992+KoulikMukherjee@users.noreply.github.com>
2024-08-27 15:41:23 +0530

    Delete WEEK 5/EXERCISE 15/BookstoreAPI directory

--- WEEK 5/EXERCISE 15/BookstoreAPI/src/main/java/com/Bookstore/bookstoreapi/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Bookstore.bookstoreapi.repository;
-
-import com.Bookstore.bookstoreapi.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- WEEK 5/EXERCISE 15/BookstoreAPI/src/main/java/com/Bookstore/bookstoreapi/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Bookstore.bookstoreapi.repository;
-
-import com.Bookstore.bookstoreapi.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
